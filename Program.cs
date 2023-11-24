48e7f7b2161d07892b0f8db8b4fcf6db35f0dbb9
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-11-24 11:45:43 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure;
+using Azure.Core;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Storage.Models;
+
+ArmClient armClient = new ArmClient(new DefaultAzureCredential());
+SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
+
+string rgName = "myRgNameLUISCOCO";
+AzureLocation location = AzureLocation.WestEurope;
+ArmOperation<ResourceGroupResource> operation = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, new ResourceGroupData(location));
+ResourceGroupResource resourceGroup = operation.Value;
+Console.WriteLine(resourceGroup.Data.Name);
